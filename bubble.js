7634cd93b81567262f20ccc60ada5a63a43720d6
Sam Courtenay <sam.m.courtenay@gmail.com>
2022-04-15 12:12:19 -0700

    finished bubble. Working on merge

--- bubble.js
+++ bubble.js
@@ -1,3 +1,16 @@
-function bubbleSort() {}
+function bubbleSort(arr) {
+    for (let i in arr) {
+        let unsortedLimit = arr.length - 1 - i;
+        for (let j = 0; j < unsortedLimit; j++) {
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+            }
+        }
+    }
+    return arr;
+}
 
 module.exports = bubbleSort;
+
+
+// [3, 2, 1]
--- merge.js
+++ merge.js
@@ -1,4 +1,17 @@
-function merge() {}
+function merge(arr1, arr2) {
+
+    const answerArr = [];
+
+    if (arr1.length > arr2.length){
+        [arr1, arr2] = [arr2, arr1];
+    }
+
+    for (let item of arr1){
+
+    }
+
+    return answerArr;
+}
 
 function mergeSort() {}
 
