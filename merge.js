f64552b113b2c6a653febf0656d513c94f31f57b
brendan <lobrendan@gmail.com>
2022-04-15 12:33:41 -0700

    working on mergeSort

--- merge.js
+++ merge.js
@@ -6,13 +6,35 @@ function merge(arr1, arr2) {
         [arr1, arr2] = [arr2, arr1];
     }
 
-    for (let item of arr1){
+    let arr1idx = 0;
+    let arr2idx = 0;
 
+    while (arr1idx < arr1.length){
+      if (arr1[arr1idx] > arr2[arr2idx]){
+        answerArr.push(arr2[arr2idx]);
+        arr2idx++;
+      } else {
+        answerArr.push(arr1[arr1idx]);
+        arr1idx++;
+      }
     }
+    answerArr.push(...arr2.slice(arr2idx, arr2.length));
 
+    console.log("answerArr : ", answerArr)
     return answerArr;
 }
 
-function mergeSort() {}
+function mergeSort(arr, arr2 = []) {
+  if (arr.length <= 1) return merge(arr, arr2);
+
+  // splitting in half
+  arr1 = arr.slice(0, Math.floor(arr.length/2));
+  arr2 = arr.slice(Math.floor(arr.length/2), arr.length);
+
+  
+
+
+  // merge
+}
 
 module.exports = { merge, mergeSort};
